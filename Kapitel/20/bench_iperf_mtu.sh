cbd608638b3379a0e4323e129384fac00e677c36
Der OPNsense-Praktiker <31452542+der-opnsense-praktiker@users.noreply.github.com>
2023-04-08 20:53:35 +0200

    4. Auflage

--- Kapitel/20/bench_iperf_mtu.sh
+++ Kapitel/20/bench_iperf_mtu.sh
@@ -1,13 +1,13 @@
 #!/bin/csh
 
-set INTERFACE=vmx1
+set INTERFACE=vtnet1
 
 foreach MTU ( 1420 1280 1024 512 256 128 )
-  echo "*** Messung mit MTU $MTU Byte"
-  ifconfig $INTERFACE mtu $MTU
+  echo "*** Messung mit MTU ${MTU} Byte"
+  ifconfig ${INTERFACE} mtu ${MTU}
   sleep 2
-  iperf3 --client 192.0.2.3 --window 128K --time 60 --interval 60 | grep bits
+  iperf3 --client 192.0.2.3 --time 60 --interval 60 --window 128K --format m | grep bit
 end
 
-ifconfig $INTERFACE mtu 1500
+ifconfig ${INTERFACE} mtu 1500
 /usr/local/etc/rc.newwanip     # neue MTU aktivieren
