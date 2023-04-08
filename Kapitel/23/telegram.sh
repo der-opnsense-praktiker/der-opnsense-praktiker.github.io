3f87011d7637fa09ad5d4f99d31fd51e31d7cbd7
Der OPNsense-Praktiker <31452542+der-opnsense-praktiker@users.noreply.github.com>
2023-04-08 20:54:46 +0200

    4. Auflage

--- Kapitel/23/telegram.sh
+++ Kapitel/23/telegram.sh
@@ -1,30 +1,20 @@
 #!/bin/csh
 # [de] Zweck: Verschicke wichtige Syslog-Meldungen an Telegram
-# [en] purpose: send important syslog messages to telegram
+# [en] Purpose: send important syslog messages to Telegram
 
 
-# [de] Zugriff zur Telegram-API
-# [en] Access credentials for the Telegram API
-set API_KEY=419840419:AAGPcJDOIe5YCS_ep3yfgPJM-f7E7m8MMMY
-set CHAT_ID=273171581
+# [de] Zugriff auf die Telegram-API
+# [en] Credentials for the Telegram API
+set API_KEY=419840419:AAGPcJDOIe5YCS_ep3yfgPJM-f7E7m81234
+set CHAT_ID=273171234
 
-# [de] ein kleines Feedback fuer Syslog
-# [en] give syslog feedback about script start
-logger -t telegram starting up
+# [de] Wie lautet die aktuelle Syslog-Meldung?
+# [en] Store the current syslog message
+set LINE = "$<"
 
-while ( 1 )
-  # [de] Wie lautet die aktuelle Syslog-Meldung?
-  # [en] Store the current syslog message
-  set LINE = "$<"
+# [de] Meldung an Telegram schicken
+# [en] Send message to Telegram
+/usr/local/bin/curl --insecure --data "chat_id=${CHAT_ID}&text=${LINE}" \
+  https://api.telegram.org/bot${API_KEY}/sendMessage
 
-  # [de] Beschreibt die Meldung einen meldenswerten Zustand?
-  # [en] compare the message with pre-defined conditions
-  if ( "$LINE" =~ "*Web GUI authentication error*" \
-    || "$LINE" =~ "*is down, removing from routing*" \
-    || "$LINE" =~ "*failed waiting for configd*" \
-    || "$LINE" =~ "Telegram-Test" ) then
-
-    /usr/local/bin/curl --insecure --data "chat_id=${CHAT_ID}&text=${LINE}" https://api.telegram.org/bot${API_KEY}/sendMessage
-  endif
-end
 exit 0
